e860406f0b542cc683fda11b6895a1cffb54eae9
Juan Manuel chico <juanmanuelchico@hotmail.com>
2021-08-27 10:07:21 -0300

    Ejercicio-07

--- Conceptos_Basicos/Ejercicio-7/Program.cs
+++ Conceptos_Basicos/Ejercicio-7/Program.cs
@@ -6,15 +6,28 @@ namespace Ejercicio_7
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Ejercicio-7");
+			Console.WriteLine("Ejercicio-7\n");
 			//Hacer un programa que pida por pantalla la fecha de nacimiento de una persona(día, mes y año) y
 			//calcule el número de días vividos por esa persona hasta la fecha actual(tomar la fecha del sistema
 			//con DateTime.Now).
 			//Nota: Utilizar estructuras selectivas.Tener en cuenta los años bisiestos.
 
+			DateTime fechaNacimiento;
+			string dia;
+			string mes;
+			string anno;
 
+			Console.Write("Ingrese Fecha de Nacimiento (AAAA/MM/DD): ");
+			
 
-
+			if (DateTime.TryParse(Console.ReadLine(), out fechaNacimiento))
+			{
+				Console.WriteLine("OK");
+			}
+			else
+			{
+				Console.WriteLine("Fecha Invalida");
+			}
 
 
 		}
