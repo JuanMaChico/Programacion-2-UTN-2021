cc99e4b952c18f5d45e55f850e00f8154df2dd6f
Juan Manuel chico <juanmanuelchico@hotmail.com>
2021-08-28 09:38:19 -0300

    Ejercicios Basicos Terminados

--- Conceptos_Basicos/Ejercicio-7/Program.cs
+++ Conceptos_Basicos/Ejercicio-7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Ejercicio_7
 {
@@ -13,23 +14,36 @@ namespace Ejercicio_7
 			//Nota: Utilizar estructuras selectivas.Tener en cuenta los años bisiestos.
 
 			DateTime fechaNacimiento;
+			DateTime fechaActual = DateTime.Today;
+			string diasVividos;
 
-			Console.Write("Ingrese Fecha de Nacimiento (AAAA/MM/DD): ");
-
+			//Bucle para validar el dato del usuario
             while (true)
             {
-				if (DateTime.TryParse(Console.ReadLine(), out fechaNacimiento))
+				//Mostramos por pantalla la interaccion con el usuario
+				Console.Write("Ingrese Fecha de Nacimiento (AAAA/MM/DD): ");
+				//Validamos el dato ingresado por el usuario
+				if (DateTime.TryParse(Console.ReadLine(), out fechaNacimiento)) 
 				{
-					Console.WriteLine("OK");
-					break;
+					//verificamos que la fecha ingresada sea menos a la fecha actual
+					if (fechaNacimiento < fechaActual)
+					{
+						//Calculamos la diferencia de dias
+						diasVividos = (fechaActual - fechaNacimiento).ToString(@"dd\d\ ");
+						//Mostramos los dias vividos al usuario
+						Console.WriteLine($"Usted a vivido {diasVividos}");
+						//Salimos del bucle
+						break;
+					}
+					else
+						//Si la fecha ingresada exede a la actual, mostramos este mensaje y volvemos a arrancar el bucle
+						Console.WriteLine("ERROR....Fecha del Futuro...");
 				}
 				else
-				{
-					Console.WriteLine("Fecha Invalida");
-				}
+					//Si la fecha no cumple con el formato mostramos este mensaje y volvemos a arrancar el bucle
+					Console.WriteLine("ERROR....Fecha Invalida");
+				
             }
-
-
 		}
 	}
 }
--- Conceptos_Basicos/Ejercicio-8/Program.cs
+++ Conceptos_Basicos/Ejercicio-8/Program.cs
@@ -10,19 +10,53 @@ namespace Ejercicio_8
 
 			//Por teclado se ingresa el valor hora, el nombre, la antigüedad(en años) y la cantidad de horas
 			//trabajadas en el mes de N empleados de una fábrica.
+			
 			//Se pide calcular el importe a cobrar teniendo en cuenta que el total(que resulta de multiplicar el
 			//valor hora por la cantidad de horas trabajadas), hay que sumarle la cantidad de años trabajados
 			//multiplicados por $ 150, y al total de todas esas operaciones restarle el 13 % en concepto de
 			//descuentos.
+	
 			//Mostrar el recibo correspondiente con el nombre, la antigüedad, el valor hora, el total a cobrar en
 			//bruto, el total de descuentos y el valor neto a cobrar de todos los empleados ingresados.
 			//Nota: Utilizar estructuras repetitivas y selectivas.
 
 
+			float valorHora;
+			string nombre;
+			int antiguedad;
+			int horasTrabajadas;
+
+			//total = [(valorHora * horasTrabajadas) + 150 * antiguedad] - 13%;
+			double liquidacionTotal;
+			//total = [(valorHora * horasTrabajadas) + 150 * antiguedad]
+			double totalBruto;
 
 
 
+			Console.Write("Ingrese valor hora del empleado: ");
+			valorHora = float.Parse( Console.ReadLine() );
+			
+			Console.Write("Ingrese nombre del empleado: ");
+			nombre = Console.ReadLine();
+			
+			Console.Write("Ingrese antiguedad del empleado: ");
+			antiguedad = int.Parse( Console.ReadLine() );
+			
+			Console.Write("Ingrese horas trabajadas en el mes: ");
+			horasTrabajadas = int.Parse( Console.ReadLine() );
 
+			liquidacionTotal = (valorHora * horasTrabajadas) + (150 * antiguedad);
+			totalBruto = liquidacionTotal;
+			double aux = liquidacionTotal * 0.13;
+			liquidacionTotal = liquidacionTotal - aux;
+
+
+			Console.WriteLine("\n\n--Recibo de sueldo--\n\n");
+			Console.WriteLine($"Empleado:  {nombre}");
+			Console.WriteLine($"antiguedad:  {antiguedad}");
+			Console.WriteLine($"Valor hora:  {valorHora}");
+			Console.WriteLine($"La liquidacion final es de: {liquidacionTotal}");
+			Console.WriteLine($"El total Bruto es de: {totalBruto}");
 
 		}
 	}
